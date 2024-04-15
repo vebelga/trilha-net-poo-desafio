cf175828a3421858085d4fd4f9c11c8d1e06d7a7
Venancio Angelo Belga <venancio.belga@autoglass.com.br>
2024-04-15 11:26:56 -0300

    Resolução desafio de código

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia(numero: "123", modelo: "m1", imei: "1654as", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("Iphone");
+Iphone iphone = new Iphone(numero: "321", modelo: "i1", imei: "dsa987", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
